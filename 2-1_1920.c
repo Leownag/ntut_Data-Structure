7631e63860e93cecd9d46069bba731122a78db87
leownag <>
2024-03-06 13:39:57 +0800

    [fix] Fix 1920.c

--- 2-1_1920.c
+++ 2-1_1920.c
@@ -1,14 +1,17 @@
 int* buildArray(int* nums, int numsSize, int* returnSize) {
     // Assign memory space for the return array.
     int *arr = malloc(numsSize * sizeof(int));
-    int *arr[numsSize];
     // Return array have the same length as the given array.
     *returnSize = numsSize;
 
-    // Get i place's value 
+    // Put nums array's nums[i] place value to retrun arr.
     for (int i = 0; i < numsSize; i++) {
         arr[i] = nums[nums[i]];
     }
     return arr;
 }
 
+/*
+BuildArray have some constant time operation and a for-loop run n(numsSize) times, time input array's lenth will affact this function's speed.
+Time Complexity: O(n)
+*/
